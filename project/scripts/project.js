4ce83e521026f0c6c2cdc690fca0780e8a38fd7f
noldie03 <pringalingling@gmail.com>
2026-02-18 09:12:08 -0700

    homepage

---
+++ project/scripts/project.js
@@ -0,0 +1,5 @@
+const year = document.getElementById('currentyear');
+year.textContent = new Date().getFullYear();
+
+const modified = document.getElementById('lastModified');
+modified.textContent = document.lastModified;
