1f1f648f41d0fb9cb27ec1980995020801bc9b71
noldie03 <pringalingling@gmail.com>
2026-02-18 14:16:18 -0700

    form and review page

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -81,3 +81,28 @@ function generateSpecsAccordion(specsArray, containerId) {
 
 // Initialize accordion
 generateSpecsAccordion(specs, "specsAccordion");
+
+
+
+
+// Only increment if this is the first load from the form submission
+if (!sessionStorage.getItem("reviewed")) {
+
+    // Get current review count from localStorage
+    let reviewCount = Number(localStorage.getItem("reviewCount")) || 0;
+
+    // Increment
+    reviewCount++;
+
+    // Save back to localStorage
+    localStorage.setItem("reviewCount", reviewCount);
+
+    // Set flag so refresh won't increment again
+    sessionStorage.setItem("reviewed", "true");
+}
+
+// Display current count
+let reviewCountDisplay = Number(localStorage.getItem("reviewCount")) || 0;
+document.getElementById("countDisplay").textContent =
+    `Total Reviews Submitted: ${reviewCountDisplay}`;
+
